31602244636736300b5f259836c38d0b75cb49be
Marini Daniel Julio <40776480+MariniDaniel@users.noreply.github.com>
2023-04-04 17:24:14 -0300

    Ejercicio15
    Calculadora

---
+++ ConsoleApp1/Calculadora/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculadora
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Funcion de calculadora
+        /// </summary>
+        /// <param name="num1">Numero 1 para la cuenta</param>
+        /// <param name="num2">Numero 2 para la cuenta</param>
+        /// <param name="operacion">Tipo de operacion a realizar + - * o /</param>
+        /// <returns></returns>
+        public static double Calcular(double num1, double num2, char operacion)
+        {
+            double resultado = 0;
+            switch (operacion)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (Validar(num2))
+                    {
+                        resultado = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se puede dividir por cero");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Operación no válida");
+                    break;
+            }
+            return resultado;
+        }
+
+        private static bool Validar(double num2)
+        {
+            return num2 != 0;
+        }
+    }
+}
---
+++ ConsoleApp1/Ejercicio15Calculadora/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ejercicio15Calculadora;
+using Calculadora;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        double numero1, numero2, resultado;
+        char operacion;
+        bool continuar = true ;
+
+        while (continuar)
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            numero2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operación (+, -, *, /):");
+            operacion = char.Parse(Console.ReadLine());
+
+            resultado = Calculadora.Calcular(numero1, numero2, operacion);
+            Console.WriteLine("El resultado es: " + resultado);
+
+            Console.WriteLine("¿Desea continuar? (s/n)");
+            char respuesta = char.Parse(Console.ReadLine());
+            if (respuesta == 'n')
+            {
+                continuar = false;
+                Console.WriteLine("Adios");
+            }
+        }
+    }
+}
