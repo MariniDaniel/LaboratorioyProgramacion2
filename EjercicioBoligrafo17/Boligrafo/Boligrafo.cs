4f2c18e2a92dbbf5a8ebd5c90fc274b266a44c2e
Marini Daniel Julio <40776480+MariniDaniel@users.noreply.github.com>
2023-04-05 22:43:56 -0300

    Se sube ejercicio 17

---
+++ EjercicioBoligrafo17/Boligrafo/Boligrafo.cs
@@ -0,0 +1,97 @@
+﻿namespace Entidades
+{
+    public class Boligrafo
+    {
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor color;
+        private short tinta;
+
+        //Constructor
+        public Boligrafo(ConsoleColor color, short tinta)
+        {
+            this.color = color;
+            this.tinta = tinta;
+        }
+
+        //getters y setters
+        //public ConsoleColor Color { get => color; set => color = value; }
+        //public short Tinta { get => tinta; set => tinta = value; }
+
+        public ConsoleColor GetColor()
+        {
+            return this.color;
+        }
+        public short GetTinta()
+        {
+            return this.tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            if (tinta > 0)
+            {
+                this.Cargar(tinta);
+            }else if(tinta < 0) 
+            {
+                this.GastarTinta(tinta);    
+            }
+        }
+
+        private void GastarTinta(short tinta)
+        {
+            int tintaAux = this.tinta - tinta;
+            if (tintaAux <= 0)
+            {
+                this.tinta = 0;
+            }else
+            {
+                this.tinta = (short) tintaAux;
+            }
+        }
+
+        /// <summary>
+        /// LO utilizo para cargar la tinta
+        /// </summary>
+        /// <param name="tinta"></param>
+        private void Cargar(short tinta)
+        {
+            //tengo la suma de estos dos lo guardo en int por si se pasa
+            int tintaAux = this.tinta + tinta;
+
+            if (tintaAux >= Boligrafo.cantidadTintaMaxima)
+            {
+                this.tinta = Boligrafo.cantidadTintaMaxima;
+            }
+            else
+            {
+                this.tinta = (short)tintaAux;
+            }
+        }
+
+        public void Recargar()
+        {
+            this.Cargar(Boligrafo.cantidadTintaMaxima);
+        }
+
+        public bool Pintar(short gasto, out string dibujo)
+        {
+            string cadenaAux = "";
+
+            if (this.tinta == 0)
+            {
+                dibujo = "No se puede pintar";
+                return false;
+            }
+
+            while (gasto > 0 && this.tinta > 0)
+            {
+                cadenaAux = cadenaAux + "*";
+                gasto--;
+                this.tinta -= 1;
+            }
+            dibujo = cadenaAux;
+            return true;
+
+        }
+    }
+}
---
+++ EjercicioBoligrafo17/EjercicioBoligrafo17/Program.cs
@@ -0,0 +1,27 @@
+﻿using Entidades;
+namespace Ejercicio17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resultado;
+            Boligrafo boligrafo = new Boligrafo(ConsoleColor.Blue,10);
+            Boligrafo boligrafo2 = new Boligrafo(ConsoleColor.Red, 50);
+
+
+            Console.WriteLine(boligrafo.GetTinta());
+            //boligrafo.Recargar(); si descomento este me imprime el resultado 15 *
+            Console.WriteLine(boligrafo.GetTinta());
+
+
+            Console.WriteLine(boligrafo2.GetTinta());
+            boligrafo2.Recargar();
+            Console.WriteLine(boligrafo2.GetTinta());
+
+            boligrafo.Pintar(59, out resultado);
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
